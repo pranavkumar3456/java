4d9c7c7e44d9dc6e52fc6f5412dd61a6ad4ba1e3
Pranav Kumar <PranavK@blts.co.za>
2024-09-05 23:21:09 +0530

    leaning java through intellij

--- primeno.java
+++ primeno.java
@@ -9,10 +9,9 @@ public class Main {
             int count = 0;
             for(int i=1;i<=N;i++)
             {            
-            if(N%i==0)
-              {
-                count = count+1;
-              }
+            if(N%i==0) {
+                count = count + 1;
+            }
             }            
             if(count==2)
               System.out.println("Number is a prime number");
