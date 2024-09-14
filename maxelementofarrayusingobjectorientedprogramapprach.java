c3e5ec3aed3d6b56358d3eca81e4c97159a74fb0
Pranav Kumar <letsgrabitpran@gmail.com>
2024-09-14 23:55:00 +0530

    array list

---
+++ maxelementofarrayusingobjectorientedprogramapprach.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class maxelementofarrayusingobjectorientedprogramapprach {
+
+    public int solve(ArrayList<Integer> A, int B){
+
+    }
+}
