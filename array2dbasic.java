8d67d478a7f42d171c3374f4930cc7933b512207
Pranav Kumar <letsgrabitpran@gmail.com>
2024-10-03 23:41:54 +0530

    basics of 2d array

---
+++ array2dbasic.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+import java.util.*;
+
+public class array2dbasic {
+    public static void main(String[] args)
+    {
+        int mat[][] = new int[4][3];
+        //4 --> no of rows
+        //3 --> no of columns.
+        //total number of rows in mtrix
+        // number of rows --> mat.length
+        // number of column --> mat[0].length
+        System.out.println(mat.length);
+        System.out.println(mat[0].length);
+    }
+
+}
