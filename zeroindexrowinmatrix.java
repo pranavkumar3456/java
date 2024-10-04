f62f1830e2bfbade3b5b86122f7951cb1dacea63
Pranav Kumar <letsgrabitpran@gmail.com>
2024-10-04 23:57:43 +0530

    zero index row in matrix

---
+++ zeroindexrowinmatrix.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+
+public class zeroindexrowinmatrix {
+
+    static void printzerorows(int mat[][])
+    {
+//        int n = mat.length;
+        int m = mat[0].length();
+        int i = 0;
+        for(int j = 0 ;j < m;j++)
+        {
+            System.out.print(mat[i][j]+"");
+        }
+
+        public static void main(String[] args)
+        {
+
+        }
+    }
+}
